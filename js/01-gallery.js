01f4ccd8a898bfb09f12c850e693b70e2fe5c34d
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-26 23:02:50 +0200

    add # 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListMarkup = document.querySelector('.gallery');
+//const galleryLink = document.querySelector('.gallery__link');
+
+
+const makeGalleryCard = ({ preview, description, original } = galleryItems) => {
+    return ` 
+<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"    />
+    </a>
+</div>`;
+};
+const galleryCards = galleryItems.map((el) => {
+    return makeGalleryCard(el);
+});
+galleryListMarkup.insertAdjacentHTML('afterbegin', galleryCards.join(''));
+
+
+
+const galleryClick = event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+
+    galleryListMarkup.addEventListener('keydown', (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        };
+    });
+};
+
+galleryListMarkup.addEventListener('click', galleryClick)
+
